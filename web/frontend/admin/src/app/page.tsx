8fe50814e1d8210da02002b3e3b9cc13be553f93
Ashutosh Sharma <ashutoshsharma.120599@gmail.com>
2026-04-11 18:50:44 +0530

    feat: created admin side application using nextjs

---
+++ web/frontend/admin/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center">
+      <h1 className="text-3xl font-bold">Admin Panel</h1>
+    </div>
+  );
+}
